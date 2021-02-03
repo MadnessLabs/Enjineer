84b5e1eca486dd1a38e96e962fb9aa2a63f0cb2c
Bobby Johnson <bobby@madnesslabs.net>
2021-02-02 21:22:10 -0600

    fix(): Update to newest version of enjin-editor and remove content wrapper from drawing demo

--- src/components/app-dashboard/app-dashboard.tsx
+++ src/components/app-dashboard/app-dashboard.tsx
@@ -86,20 +86,18 @@ export class AppDashboard implements ComponentInterface {
 
   render() {
     return [
-      <ion-content>
-        <canvas
-          touch-action="none"
-          id="board"
-          width="640"
-          height="640"
-          style={{
-            width: "320px",
-            height: "320px",
-          }}
-        >
-          Opps, you cannot play draw N guess with this browser!
-        </canvas>
-      </ion-content>,
+      <canvas
+        touch-action="none"
+        id="board"
+        width="640"
+        height="640"
+        style={{
+          width: "320px",
+          height: "320px",
+        }}
+      >
+        Opps, you cannot play draw N guess with this browser!
+      </canvas>,
     ];
   }
 }
